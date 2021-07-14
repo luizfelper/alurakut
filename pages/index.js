6fb6d3b225029a5a100381421d020b192caee45b
Luiz Felipe <luizfelper@gmail.com>
2021-07-13 22:00:48 -0300

    InicioAluraKut

---
+++ pages/index.js
@@ -0,0 +1,66 @@
+/* import styled from 'styled-components' */
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+
+function ProfileSideBar(propriedades) {
+  console.log(propriedades);
+  return (
+    <Box>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
+
+
+export default function Home() {
+
+  const githubUser = 'luizfelper';
+  const pessoasFavoritas =['juunegreiros',
+  'omariosouto',
+  'rafaballerini',
+  'marcobrunodev',
+  'luizfelper',
+  'felipefialho'];
+
+  return (
+    <>
+    <AlurakutMenu />
+      <MainGrid>
+        {/* <Box style="grid-area: profileArea;"> Transforma a estilização mais dinâmica. Ex.: (#box {grid-area: profileArea;}) Fica: style={{ gridArea: 'profileArea'}} */} 
+        <div className="profileArea" style={{ gridArea: 'profileArea'}}>
+        <ProfileSideBar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a)
+
+              <OrkutNostalgicIconSet />
+            </h1>
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea'}}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Galera da comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                        <img src={`https://github.com/${itemAtual}.png`} />
+                        <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
+}
