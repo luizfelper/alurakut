f3e5f27c7b05902f5371630b7825fb3081f5dec5
Luiz Felipe <luizfelper@gmail.com>
2021-09-26 01:56:59 -0300

    Configurações de Token

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import nookies from 'nookies'
 
 export default function LoginScreen() {
     const router = useRouter();
-    const [gitHubUser, setGitHubUser] = React.useState('');
+    const [githubUser, setGitHubUser] = React.useState('');
 
     return (
         <main style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -19,49 +20,64 @@ export default function LoginScreen() {
                 <section className="formArea">
                     <form className="box" onSubmit={(e) => {
                         e.preventDefault();
-                        console.log('Usuario: ', gitHubUser);
-                        router.push('/');
+                        fetch('https://alurakut.vercel.app/api/login', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({ githubUser: 'omariosouto' })
+                        })
+                            .then(async (respostaDoServer) => {
+                                const dadosDaResposta = await respostaDoServer.json()
+                                const token = dadosDaResposta.token;
+                                console.log(token)
+                        nookies.set(null, 'USER_TOKEN', token, {
+                            path: '/',
+                            maxAge: 86400 * 7
+                        });
+                        router.push('/')
+                    })
                     }}>
-                        <p>
-                            Acesse agora mesmo com seu usuário do <strong>GitHub</strong>!
-                        </p>
-                        <input
-                            placeholder="Usuário"
-                            value={gitHubUser}
-                            onChange={(event) => {
-                                setGitHubUser(event.target.value);
-                            }}
-                        />
-                            {gitHubUser.length === 0 //O if dentro do React não funciona, vai ter que usar ternário
-                            ? 'Preencha o campo' // Se for igual a zero mostra essa mensagem
-                            : '' // Senão...
-                        } 
-                        <button onClick={(e) => {
-                            e.preventDefault();
-                            router.push('/', {});
-                        }} type="submit">
-                            Login
-                        </button>
-                    </form>
-
-                    <footer className="box">
-                        <p>
-                            Ainda não é membro? <br />
-                            <a href="/login">
-                                <strong>
-                                    ENTRAR JÁ
-                                </strong>
-                            </a>
-                        </p>
-                    </footer>
-                </section>
-
-                <footer className="footerArea">
                     <p>
-                        © 2021 alura.com.br - <a href="/">Sobre o Orkut.br</a> - <a href="/">Centro de segurança</a> - <a href="/">Privacidade</a> - <a href="/">Termos</a> - <a href="/">Contato</a>
+                        Acesse agora mesmo com seu usuário do <strong>GitHub</strong>!
+                    </p>
+                    <input
+                        placeholder="Usuário"
+                        value={githubUser}
+                        onChange={(event) => {
+                            setGitHubUser(event.target.value);
+                        }}
+                    />
+                    {githubUser.length === 0 //O if dentro do React não funciona, vai ter que usar ternário
+                        ? 'Preencha o campo' // Se for igual a zero mostra essa mensagem
+                        : '' // Senão...
+                    }
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        router.push('/', {});
+                    }} type="submit">
+                        Login
+                    </button>
+                </form>
+
+                <footer className="box">
+                    <p>
+                        Ainda não é membro? <br />
+                        <a href="/login">
+                            <strong>
+                                ENTRAR JÁ
+                            </strong>
+                        </a>
                     </p>
                 </footer>
-            </div>
-        </main>
+            </section>
+
+            <footer className="footerArea">
+                <p>
+                    © 2021 alura.com.br - <a href="/">Sobre o Orkut.br</a> - <a href="/">Centro de segurança</a> - <a href="/">Privacidade</a> - <a href="/">Termos</a> - <a href="/">Contato</a>
+                </p>
+            </footer>
+        </div>
+        </main >
     );
 }
